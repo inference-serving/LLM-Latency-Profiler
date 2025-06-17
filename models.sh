6261f0b836c0a3bf3c09152bce637fea82c5f4e5
Saeid Ghafouri <s.ghafouri@qub.ac.uk>
2025-06-17 17:03:56 +0100

    new bench

--- models.sh
+++ models.sh
@@ -8,8 +8,8 @@ MODELS=(
   # "facebook/opt-350m"
   # "tiiuae/falcon-rw-1b"
   # "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-  meta-llama/Llama-3.2-1B
-  meta-llama/Llama-3.2-3B
+  meta-llama/Llama-3.2-1B-Instruct
+  # meta-llama/Llama-3.2-3B
 )
 
 
