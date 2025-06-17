f87a6ae3d622b9d77fc84302cf613ce57fdb7f20
Saeid Ghafouri <s.ghafouri@qub.ac.uk>
2025-06-17 10:15:34 +0100

    llama 1b GPU results added

--- models.sh
+++ models.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 MODELS=(
-  "gpt2"
-  "distilgpt2"
-  "sshleifer/tiny-gpt2"
-  "facebook/opt-125m"
-  "facebook/opt-350m"
-  "tiiuae/falcon-rw-1b"
-  "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+  # "gpt2"
+  # "distilgpt2"
+  # "sshleifer/tiny-gpt2"
+  # "facebook/opt-125m"
+  # "facebook/opt-350m"
+  # "tiiuae/falcon-rw-1b"
+  # "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+  meta-llama/Llama-3.2-1B
+  meta-llama/Llama-3.2-3B
 )
 
 
